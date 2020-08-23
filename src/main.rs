b842bc081709dd8b7202e90e95511b5cd3d4c791
RichoDemus <git@richodemus.com>
2020-08-23 21:25:57 +0200

    Basic gravity done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use legion::prelude::*;
-use rand::Rng;
-use ggez::{graphics, Context, ContextBuilder, GameResult, timer};
+use ggez::{Context, ContextBuilder, GameResult, graphics, timer};
+use ggez::conf::{FullscreenType, WindowMode, WindowSetup};
 use ggez::event::{self, EventHandler};
 use ggez::nalgebra::Point2;
-use ggez::conf::{WindowMode, FullscreenType, WindowSetup};
+use legion::prelude::*;
 use nalgebra::Vector2;
+use rand::Rng;
 
 // Define our entity data types
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Position {
-    x: f32,
-    y: f32,
+    vector: Vector2<f32>
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -21,12 +20,16 @@ struct MyVector2 {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Velocity {
-    x: f32,
-    y: f32,
+    vector: Vector2<f32>
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Mass(f32);
+
+#[derive(Clone, Debug, PartialEq)]
+struct Data {
+    name: String
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Model(usize);
@@ -37,6 +40,7 @@ struct Static;
 const WIDTH: f32 = 800.0;
 const HEIGHT: f32 = 600.0;
 const NUM_BODIES: i32 = 100;
+const GRAVITATIONAL_CONSTANT: f32 = 1.0f32;
 
 fn main() {
 
@@ -67,7 +71,7 @@ fn main() {
             min_height: 0.0,
             max_width: 0.0,
             max_height: 0.0,
-            resizable: false
+            resizable: false,
         })
         .window_setup(window_setup)
         .build()
@@ -83,12 +87,10 @@ fn main() {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured: {}", e)
     }
-
-
 }
 
 struct MyGame {
-    universe: Universe,
+    // universe: Universe,
     world: World,
 }
 
@@ -100,22 +102,31 @@ impl MyGame {
         let mut rng = rand::thread_rng();
         world.insert(
             (),
-            (0..NUM_BODIES).map(|_| (
-                Position { x: rng.gen_range(0., WIDTH), y: rng.gen_range(0., HEIGHT) },
-                Velocity { x: 0.0, y: 0.0 },
-                Mass(1.)
-            )),
+            (0..NUM_BODIES).map(|i| {
+                let x = rng.gen_range(0., WIDTH);
+                let y = rng.gen_range(0., HEIGHT);
+
+                let x_velocity = rng.gen_range(-1., 1.);
+                let y_velocity = rng.gen_range(-1., 1.);
+
+                (
+                    Data { name: i.to_string() },
+                    Position { vector: Vector2::new(x, y) },
+                    Velocity { vector: Vector2::new(x_velocity, y_velocity) },
+                    Mass(1.)
+                )
+            }),
         );
 
         MyGame {
-            universe,
+            // universe,
             world,
         }
     }
 
     pub fn draw_fps_counter(&self, ctx: &mut Context) -> GameResult<()> {
         let fps = timer::fps(ctx);
-        let delta = timer::delta(ctx);
+        // let delta = timer::delta(ctx);
         // let stats_display = graphics::Text::new(format!("FPS: {:.0}, delta: {:?}", fps, delta));
         let stats_display = graphics::Text::new(format!("FPS: {:.0}", fps));
         // println!(
@@ -133,31 +144,80 @@ impl MyGame {
 }
 
 impl EventHandler for MyGame {
-    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-
-        let mut query = <(Write<Position>, Write<Velocity>, Read<Mass>)>::query();
-
-// Iterate through all entities that match the query in the world
-        let dt = timer::delta(ctx).as_secs_f32();
-        for (mut pos, mut vel, mass) in query.iter_mut(&mut self.world) {
-            let force = MyVector2 { x: 0.0, y: mass.0 * 9.81 };
-            let acceleration = MyVector2 { x: force.x / mass.0, y: force.y / mass.0 };
-            vel.x += acceleration.x * dt;
-            vel.y += acceleration.y * dt;
-            pos.x += vel.x * dt;
-            pos.y += vel.y * dt;
-        }
-
-        let mut query = <(Read<Position>)>::query();
-
-        let entities_to_delete = query.iter_entities(&mut self.world)
-            .filter(|(_, pos)| pos.y > HEIGHT)
-            .map(|(entity,_)|entity)
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        let query = <(Read<Position>, Read<Data>)>::query();
+        let bodies = query.iter(&self.world)
+            .map(|(pos, data)| (*pos, data.as_ref().clone()))
             .collect::<Vec<_>>();
 
-        entities_to_delete.into_iter().for_each(|entity| {
+        let query = <(Read<Position>, Write<Velocity>, Read<Data>)>::query();
+        for (position, mut velocity, data) in query.iter_mut(&mut self.world) {
+            for (other_position, other_data) in &bodies {
+                let data: &Data = &data;
+                let other_data: &Data = other_data;
+                if data == other_data {
+                    continue;
+                }
+                let position: &Position = &position;
+                // println!("\t\tComparing body {:?}: {:?} with {:?}: {:?}", data, position, other_data, other_position);
+
+
+                let difference: Vector2<f32> = &other_position.vector - &position.vector;
+                let distance = difference.magnitude();
+                let gravity_direction: Vector2<f32> = difference.normalize();
+                let gravity: f32 = GRAVITATIONAL_CONSTANT * (1. * 1.) / (distance * distance);
+
+                // *velocity.vector = *velocity.vector + *velocity.vector;
+
+                let force: Vector2<f32> = gravity_direction * gravity;
+                // let force: Vector2<f32> = force.into();
+                let original_vector: Vector2<f32> = velocity.vector.clone_owned();
+                let new_vector: Vector2<f32> = original_vector + force;
+                // println!("\tgravitational force: {:?}", force);
+                // let vel:Vector2<f32> = *velocity.vector + force;
+                // velocity.vector = vel;
+                velocity.vector = new_vector;
+                // println!("\ttotal force: {:?}", *velocity);
+            }
+        }
+
+        let query = <(Write<Position>, Read<Velocity>)>::query();
+        for (mut position, velocity) in query.iter_mut(&mut self.world) {
+            let current_position: Vector2<f32> = position.vector.clone_owned();
+            let velocity: Vector2<f32> = velocity.vector.clone_owned();
+
+            let new_position: Vector2<f32> = current_position + velocity;
+
+            position.vector = new_position;
+        }
+
+        // collision detection
+        let query = <(Read<Position>, Read<Data>)>::query();
+        let bodies = query.iter(&self.world)
+            .map(|(pos, data)| (*pos, data.as_ref().clone()))
+            .collect::<Vec<_>>();
+
+        let query = <(Read<Position>, Write<Velocity>, Read<Data>)>::query();
+        let mut entities_to_delete = vec![];
+        for (entity, (position, _velocity, data)) in query.iter_entities_mut(&mut self.world) {
+            for (other_position, other_data) in &bodies {
+                let data: &Data = &data;
+                let other_data: &Data = other_data;
+                if data == other_data {
+                    continue;
+                }
+
+                let difference: Vector2<f32> = &other_position.vector - &position.vector;
+                let distance = difference.magnitude();
+                if distance < 0.99 {
+                    entities_to_delete.push(entity);
+                }
+            }
+        }
+
+        for entity in entities_to_delete {
             self.world.delete(entity);
-        });
+        }
 
         Ok(())
     }
@@ -166,8 +226,8 @@ impl EventHandler for MyGame {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
         self.draw_fps_counter(ctx)?;
 
-        let query = <(Read<Position>)>::query();
-        for pos in query.iter((&self.world)) {
+        let query = <Read<Position>>::query();
+        for pos in query.iter(&self.world) {
             let circle = graphics::Mesh::new_circle(
                 ctx,
                 graphics::DrawMode::fill(),
@@ -176,9 +236,26 @@ impl EventHandler for MyGame {
                 2.0,
                 graphics::WHITE,
             )?;
-            graphics::draw(ctx, &circle, (Point2::new(pos.x, pos.y),))?;
+            graphics::draw(ctx, &circle, (Point2::new(pos.vector.x, pos.vector.y), ))?;
         }
 
         graphics::present(ctx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let vector: Vector2<f32> = Vector2::new(11., 11.);
+        let vector1 = Vector2::new(10., 10.);
+
+        let result: Vector2<f32> = vector1 - vector;
+
+        let result = result.magnitude();
+
+        print!("{:?}", result)
     }
 }
